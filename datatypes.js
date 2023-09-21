c1a0900ed449e7d6cb2dc0be7254354dfd2db9bc
SUMAN KUMAR RAWANI <136788566+Kumar9534@users.noreply.github.com>
2023-09-21 22:17:06 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,36 @@
+console.log(typeof 10);
+console.log(typeof 'suman');
+console.log(typeof true);
+console.log(typeof undecleredVariable);
+
+
+var x="";
+var y="";
+var z= x+y;
+console.log(z);
+
+var x='2';
+var y='3';
+var z='4';
+var p='5';
+var Q= x+y*z+p;
+console.log(Q);
+
+
+let firstName = 'suman'
+let lastName = 'Rawani'
+let country = 'Bharat'
+let age = '20'
+let marital='No'
+console.log(firstName,lastName,country,age,marital);
+
+
+console.log(firstName);
+console.log(lastName);
+console.log(country);
+console.log(age);
+console.log(marital);
+
+let myAge = 'i am 20 years old'
+let yourAge =' you are 30 years old'
+console.log(myAge,yourAge);
---
+++ variable.js
@@ -0,0 +1,33 @@
+// comment can make code readable
+// welcome  to 30daysof javaScript
+// comments can make code readable, 
+// easy to reuse and informative
+
+//  variable-----------------
+var x= 5;
+var y= 6;
+var z= x+y;
+console.log(z);
+// ----------------------string----------
+const string1= "A string primitive";
+const string2 ="Also a string primitive";
+console.log(string1,string2);
+
+// -----------------Boolean------------------
+function gfg() {
+    console.log(Boolean(12));
+}
+gfg();
+
+// undefine and null data type---------------
+var temp=['a','b','c'];
+if(temp[3] === undefined)
+console.log("true");
+else
+console.log("false");
+
+var temp=['a','b','c','d'];
+if(temp[3] === undefined)
+console.log("true");
+else
+console.log("false");
